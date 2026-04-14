b7fbca870abc29429a3dcc1a46a23e072196795d
HarshSPanchal <panchal.harsh2022@gmail.com>
2026-04-14 17:17:35 +0530

    Fixing the things

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -32,7 +32,7 @@ const useGames = (
       getNextPageParam: (lastPage, allPages) => {
         return lastPage.next ? allPages.length + 1 : undefined;
       },
-      staleTime: 24 * 60 * 60 * 1000//24hr
+      staleTime: 24 * 60 * 60 * 1000,//24hr
   })
 
 export default useGames;
