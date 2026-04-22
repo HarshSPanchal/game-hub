496bbb91216b926c58a9ae5623d796d1d6f00f9b
HarshSPanchal <panchal.harsh2022@gmail.com>
2026-04-22 22:32:42 +0530

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 
 const GameDetailPage = () => {
@@ -18,8 +19,9 @@ const GameDetailPage = () => {
     <>
       <Heading>{ game.name }</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game}/>
-      <GameTrailer gameId={game.id}/>
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   )
 }
